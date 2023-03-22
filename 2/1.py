a3644f0f084a03e5c1a89d5b3e2c732846a17b7f
More3333 <126098045+More3333@users.noreply.github.com>
2023-03-23 01:20:14 +0700

    Add files via upload

---
+++ 2/1.py
@@ -0,0 +1,13 @@
+
+
+
+]
+if x == 0:
+    x= 1
+    print("x был равен нулю")
+elif type(x) == type(5) or type(x) == type (5.3):
+    print("x допустимое значение")
+else:
+    print("В X не допустимый ти данных")
+    x = 1
+print(100/x)
